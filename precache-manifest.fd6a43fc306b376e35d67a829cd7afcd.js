a2899d82adc7f14b9539788ef8586232981c06ec
Vladoss20 <54867207+Vladoss20@users.noreply.github.com>
2019-11-13 16:39:45 +0100

    Updates

---
+++ precache-manifest.fd6a43fc306b376e35d67a829cd7afcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3006f86820fbf19339c751618396e950",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "74c41fc385795356be99",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "36fdb235b33bd6882edf",
+    "url": "/robofriends/static/css/main.dc936091.chunk.css"
+  },
+  {
+    "revision": "74c41fc385795356be99",
+    "url": "/robofriends/static/js/2.964031dd.chunk.js"
+  },
+  {
+    "revision": "36fdb235b33bd6882edf",
+    "url": "/robofriends/static/js/main.1e9e0cfb.chunk.js"
+  },
+  {
+    "revision": "37773780271047ce06e4",
+    "url": "/robofriends/static/js/runtime-main.55723ab0.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
